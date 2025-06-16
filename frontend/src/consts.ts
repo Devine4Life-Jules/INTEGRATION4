a647b197993d6e762939f7f1e034c7ace19802c0
JulesDevine <jules.desmet@student.howest.be>
2025-06-16 10:43:29 +0200

    connected to back end, added animation, changed link and button

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = "Abby Key";
 export const SITE_DESCRIPTION = "Welcome to my website!";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://julesdesmet.be/drupal";
 export const POPUP_RESET_TIME = 5000;
 export const RESET_TIME = 10000;
